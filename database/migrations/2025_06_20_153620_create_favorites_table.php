abb5b14c352dec609b89f2ee00441a00abc7b3f8
Baraa <baraa.aladeeb@gmail.com>
2025-06-22 01:27:31 +0300

    correct error in favorites table

--- database/migrations/2025_06_20_153620_create_favorites_table.php
+++ database/migrations/2025_06_20_153620_create_favorites_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users' , 'id')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('announcement_id')->constrained('announcements' . 'id')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('announcement_id')->constrained('announcements' , 'id')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
