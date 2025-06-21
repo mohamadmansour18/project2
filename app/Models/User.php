67bc5cb7235f4f3f54fb636f889550a3cfaca62c
Mohammed Mansour <360mohamad360@gmail.com>
2025-06-21 23:33:27 +0300

    test

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,5 +71,83 @@ class User extends Authenticatable implements JWTSubject
         return [];
     }
 
+    public function Profile(): HasOne
+    {
+        return $this->hasOne(Profile::class, 'user_id', 'id');
+    }
 
+    public function OtpCodes(): HasMany
+    {
+        return $this->hasMany(OtpCode::class , 'user_id' , 'id');
+    }
+
+    public function Announcements(): BelongsToMany
+    {
+        return $this->belongsToMany(Announcements::class , 'favorites' , 'user_id' , 'announcement_id');
+    }
+
+    public function sendInvitations(): HasMany
+    {
+        return $this->hasMany(GroupInvitation::class , 'invited_by_user_id' , 'id');
+    }
+
+    public function receivedInvitations(): HasMany
+    {
+        return $this->hasMany(GroupInvitation::class , 'invited_user_id' , 'id');
+    }
+
+    public function groupMember(): HasOne
+    {
+        return $this->hasOne(GroupMember::class , 'user_id' , 'id');
+    }
+
+    public function projectForms(): HasMany
+    {
+        return $this->hasMany(ProjectForm::class , 'user_id' , 'id');
+    }
+
+    public function joinRequest(): HasMany
+    {
+        return $this->hasMany(JoinRequest::class , 'user_id' , 'id');
+    }
+
+    public function doctorInquiries(): HasMany
+    {
+        return $this->hasMany(DoctorInquiry::class , 'doctor_id' , 'id');
+    }
+
+    public function formSignature(): HasOne
+    {
+        return $this->hasOne(FormSignature::class , 'user_id' , 'id');
+    }
+
+    public function supervisor(): HasOne
+    {
+        return $this->hasOne(InterviewCommittee::class , 'supervisor_id' , 'id');
+    }
+
+    public function member(): HasOne
+    {
+        return $this->hasOne(InterviewCommittee::class , 'member_id' , 'id');
+    }
+
+    public function gradeException(): HasOne
+    {
+        return $this->hasOne(GradeException::class , 'student_id' , 'id');
+    }
+
+    public function FirstPartyConversation(): HasMany
+    {
+        return $this->hasMany(Conversation::class , 'user_one_id' , 'id');
+    }
+
+    public function secondPartyConversation(): HasMany
+    {
+        return $this->hasMany(Conversation::class , 'user_two_id' , 'id');
+    }
+
+    public function Messages(): HasMany
+    {
+        return $this->hasMany(Message::class , 'sender_id' , 'id');
+    }
 }
