2e18bf40d7ce0853280fcce9549e27942efc2ce4
Mohammed Mansour <360mohamad360@gmail.com>
2025-06-25 21:12:00 +0300

    install laravel horizon package

--- app/Jobs/SendOtpCodeJob.php
+++ app/Jobs/SendOtpCodeJob.php
@@ -8,12 +8,15 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class SendOtpCodeJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public $tries = 2 ;
+    public $timeout = 10 ;
     public string $email ;
     public string $name ;
     public string $otp ;
@@ -33,5 +36,11 @@ class SendOtpCodeJob implements ShouldQueue
     public function handle(): void
     {
         Mail::to($this->email)->send(new SendOtpMail($this->otp , $this->name));
+    }
+
+    public function failed(\Throwable $exception): void
+    {
+        Log::error('Send OTP email job failed for user : ' . $this->email . 'has name : ' . $this->name);
+        Log::error($exception->getMessage());
     }
 }
