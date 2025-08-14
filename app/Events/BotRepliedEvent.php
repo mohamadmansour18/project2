62819206625f2daed80b10421f0132ab792dd856
Mohammed Mansour <360mohamad360@gmail.com>
2025-08-14 18:30:13 +0300

    add pusher configration with event broadcast

---
+++ app/Events/BotRepliedEvent.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class BotRepliedEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public int $conversationId,
+        public array $payload,
+    )
+    {}
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('conversation.'.$this->conversationId),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'bot.replied';
+    }
+
+    public function broadcastWith(): array
+    {
+        return $this->payload;
+    }
+}
